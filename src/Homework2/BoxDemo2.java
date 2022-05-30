61239a0710e29f91c27d5e94560d46318145fe1b
HP <nare.vos90@mail.ru>
2022-05-30 17:02:42 +0300

    Initial commit

---
+++ src/Homework2/BoxDemo2.java
@@ -0,0 +1,19 @@
+package Homework2;
+
+public class BoxDemo2 {
+    public static void main(String[] args) {
+    }
+
+    double width;
+
+    double heigth;
+    double depth;
+
+    void volium() {
+        System.out.print("abyom ravno");
+        System.out.println(width * heigth * depth);
+
+
+    }
+
+}
